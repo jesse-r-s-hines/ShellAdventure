4f124d1a4cd5cceec7206fb586e7430e5af747fa
Jesse Hines <jessehines@southern.edu>
2021-02-07 11:33:49 -0500

    Refactor test_tutorial

--- tests/test_tutorial.py
+++ tests/test_tutorial.py
@@ -1,3 +1,4 @@
+from typing import *
 import pytest
 from pytest import mark
 from shell_adventure.tutorial import Tutorial
@@ -25,20 +26,25 @@ SIMPLE_TUTORIAL = """
 """
 
 class TestTutorial:
-    def test_creation(self, tmp_path):
-        puzzles = tmp_path / "mypuzzles.py"
-        puzzles.write_text(SIMPLE_PUZZLES)
+    def _create_tutorial(tmp_path, puzzles: Dict[str, str], config: str):
+        """
+        Creates a tutorial with the given puzzles and config strings.
+        Config will be saved to tmp_path/myconfig.py, puzzles will be saved to the dictionary key names under tmp_path.
+        """
+        for name, content in puzzles.items():
+            (tmp_path / name).write_text(content)
+        config_file = tmp_path / "myconfig.yaml"
+        config_file.write_text(config)
+        tutorial = Tutorial(config_file)
+        return tutorial
 
-        config = tmp_path / "tutorial.yaml"
-        config.write_text(f"""
+    def test_creation(self, tmp_path):
+        tutorial = TestTutorial._create_tutorial(tmp_path, {"mypuzzles.py": SIMPLE_PUZZLES}, f"""
             modules:
-                - {puzzles}
+                - {tmp_path / "mypuzzles.py"}
             puzzles:
                 - mypuzzles.move
         """)
-
-        tutorial = Tutorial(config)
-        assert tutorial.config_file == config
 
         # Should contain the default module and my module
         assert set(tutorial.modules.keys()) == {"default", "mypuzzles"}
@@ -49,20 +55,16 @@ class TestTutorial:
         assert tutorial.puzzles[0].puzzle == None # Not generated yet
 
     def test_relative_path_creation(self, tmp_path):
-        (tmp_path / "mypuzzles.py").write_text(SIMPLE_PUZZLES)
-
-        config = tmp_path / "tutorial.yaml"
-        config.write_text(SIMPLE_TUTORIAL)
-
-        tutorial = Tutorial(f"{config}") # Strings should also work for path
+        tutorial = TestTutorial._create_tutorial(tmp_path, {"mypuzzles.py": SIMPLE_PUZZLES}, SIMPLE_TUTORIAL)
+        tutorial = Tutorial(f"{tmp_path / 'myconfig.yaml'}") # Strings should also work for path
+        assert tutorial.config_file == tmp_path / "myconfig.yaml"
         assert "mypuzzles.move" in tutorial.generators
 
     def test_multiple_modules(self, tmp_path):
-        (tmp_path / "mypuzzles1.py").write_text(SIMPLE_PUZZLES)
-        (tmp_path / "mypuzzles2.py").write_text(SIMPLE_PUZZLES)
-
-        config = tmp_path / "tutorial.yaml"
-        config.write_text(f"""
+        tutorial = TestTutorial._create_tutorial(tmp_path, {
+            "mypuzzles1.py": SIMPLE_PUZZLES,
+            "mypuzzles2.py": SIMPLE_PUZZLES,
+        }, """
             modules:
                 - mypuzzles1.py
                 - mypuzzles2.py
@@ -70,38 +72,26 @@ class TestTutorial:
                 - mypuzzles1.move
         """)
 
-        tutorial = Tutorial(config)
         assert "mypuzzles1.move" in tutorial.generators
         assert "mypuzzles2.move" in tutorial.generators
 
-    def test_missing_module(self, tmp_path):
-        config = tmp_path / "tutorial.yaml"
-        config.write_text(SIMPLE_TUTORIAL)
-
+    def test_missing_files(self, tmp_path):
         with pytest.raises(FileNotFoundError):
-            tutorial = Tutorial(config)
+            tutorial = TestTutorial._create_tutorial(tmp_path, {}, SIMPLE_TUTORIAL) # Don't make any puzzle files
+        with pytest.raises(FileNotFoundError):
+            tutorial = Tutorial(tmp_path / "not_a_config_file.yaml")
 
     def test_missing_puzzle(self, tmp_path):
-        config = tmp_path / "tutorial.yaml"
-        config.write_text(f"""
-            puzzles:
-                - default.not_a_puzzle_name
-        """)
-
-        with pytest.raises(AssertionError, match="Unknown puzzle generator default.not_a_puzzle_name"):
-            tutorial = Tutorial(config)
-
+        with pytest.raises(AssertionError, match="Unknown puzzle generator mypuzzles.not_a_puzzle"):
+            tutorial = TestTutorial._create_tutorial(tmp_path, {"mypuzzles.py": SIMPLE_PUZZLES}, """
+                modules:
+                    - mypuzzles.py
+                puzzles:
+                    - mypuzzles.not_a_puzzle
+            """)
 
     def test_solve_puzzle(self, tmp_path):
-        (tmp_path / "mypuzzles.py").write_text(SIMPLE_PUZZLES)
-        config = tmp_path / "tutorial.yaml"
-        config.write_text(f"""
-            modules:
-                - mypuzzles.py
-            puzzles:
-                - mypuzzles.move
-        """)
-        tutorial = Tutorial(config)
+        tutorial = TestTutorial._create_tutorial(tmp_path, {"mypuzzles.py": SIMPLE_PUZZLES}, SIMPLE_TUTORIAL)
         tutorial.run()
         puzzle = tutorial.puzzles[0].puzzle
 
@@ -116,22 +106,17 @@ class TestTutorial:
         assert puzzle.solved == True
 
     def test_solve_puzzle_feedback(self, tmp_path):
-        (tmp_path / "mypuzzles.py").write_text("""
-def unsolvable(file_system):
-    return Puzzle(
-        question = f"You can never solve this puzzle.",
-        checker = lambda: "Unsolvable!",
-    )
-        """)
-
-        config = tmp_path / "tutorial.yaml"
-        config.write_text(f"""
+        puzzles = """def unsolvable(file_system):
+                        return Puzzle(
+                            question = f"You can never solve this puzzle.",
+                            checker = lambda: "Unsolvable!",
+                        )"""
+        tutorial = TestTutorial._create_tutorial(tmp_path, {"mypuzzles.py": puzzles}, """
             modules:
                 - mypuzzles.py
             puzzles:
                 - mypuzzles.unsolvable
         """)
-        tutorial = Tutorial(config)
         tutorial.run()
         puzzle = tutorial.puzzles[0].puzzle
 
@@ -139,25 +124,20 @@ def unsolvable(file_system):
         assert puzzle.solved == False
 
     def test_solve_puzzle_error(self, tmp_path):
-        (tmp_path / "mypuzzles.py").write_text("""
-def invalid(file_system):
-    return Puzzle(
-        question = f"This puzzle is invalid",
-        checker = lambda: 100,
-    )
-        """)
-
-        config = tmp_path / "tutorial.yaml"
-        config.write_text(f"""
+        puzzles = """def invalid(file_system):
+                        return Puzzle(
+                            question = f"This puzzle is invalid",
+                            checker = lambda: 100,
+                        )"""
+        tutorial = TestTutorial._create_tutorial(tmp_path, {"mypuzzles.py": puzzles}, """
             modules:
                 - mypuzzles.py
             puzzles:
                 - mypuzzles.invalid
         """)
-        tutorial = Tutorial(config)
         tutorial.run()
+        
         puzzle = tutorial.puzzles[0].puzzle
-
         with pytest.raises(Exception, match="bool or str expected"):
             tutorial.solve_puzzle(puzzle)
 
