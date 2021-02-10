49ff89e93ef41fbd327cd88fcbbfc0e7d3c8ec80
Jesse Hines <jessehines@southern.edu>
2021-02-10 15:05:39 -0500

    Move tutorials folder

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker build -t shell-adventure . 1> /dev/null && # Only print errors
-python3.7 -m shell_adventure.shell_adventure shell_adventure/tutorials/default.yaml 
+python3.7 -m shell_adventure.shell_adventure tutorials/default.yaml 
