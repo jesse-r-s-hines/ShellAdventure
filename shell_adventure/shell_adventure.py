be178b5f9fc94894d09b0611a6a831a7dd2d97ed
Jesse Hines <jessehines@southern.edu>
2021-03-08 21:52:04 -0500

    Connect to bash in actual app.

--- shell_adventure/shell_adventure.py
+++ shell_adventure/shell_adventure.py
@@ -15,4 +15,5 @@ if __name__ == "__main__":
 
     with Tutorial(sys.argv[1]) as tutorial: # Creates and sets up the container with the tutorial inside
         bash = start_bash(tutorial.container)
+        tutorial.connect_to_bash()
         gui = GUI(tutorial)
