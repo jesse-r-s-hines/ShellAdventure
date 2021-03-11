5652982f6348ddff5f5e6b389eaa9498d882b126
Jesse Hines <jessehines@southern.edu>
2021-03-11 18:41:15 -0500

    Use venv

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 docker build -t shell-adventure . 1> /dev/null && # Only print errors
-python3.7 -m shell_adventure.shell_adventure tutorials/default.yaml 
+source .venv/bin/activate
+.venv/bin/python3.7 -m shell_adventure.shell_adventure tutorials/default.yaml 
--- tests.sh
+++ tests.sh
@@ -2,13 +2,15 @@
 docker build -t shell-adventure . 1> /dev/null # Only print errors
 docker build -t shell-adventure:test --build-arg TESTING=1 . 1> /dev/null # Only print errors
 
+source .venv/bin/activate
+
 echo "=========== mypy analysis ==========="
 mypy shell_adventure shell_adventure_docker tests
 
 # See https://docs.pytest.org/en/documentation-restructure/how-to/usage.html#possible-exit-codes for meaning of pytest exit codes
 echo -e "\n\n"
 echo "Main tests"
-python3.7 -m pytest --cov --cov-report term
+.venv/bin/python3.7 -m pytest --cov --cov-report term
 
 TEST_DIR=/usr/local/shell_adventure_docker_tests
 echo -e "\n\n"
