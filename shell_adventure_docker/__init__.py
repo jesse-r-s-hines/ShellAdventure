391ab749d03b162ae62e35d004543a527dfe66a5
Jesse Hines <jessehines@southern.edu>
2021-06-13 17:32:42 -0500

    Add __all__ to shell_adventure_docker package

--- shell_adventure_docker/__init__.py
+++ shell_adventure_docker/__init__.py
@@ -1,20 +1,29 @@
-# imports will be part of the package namespace, so you can use shell_adventure_docker.File, etc
 from .support import Puzzle
 from .file import File
 from .permissions import Permissions, change_user
-from .random_helper import RandomHelper as _RandomHelper # private import
-from .tutorial_docker import TutorialDocker as _TutorialDocker # private import
-from pathlib import Path as _Path # private import
+from .random_helper import RandomHelper
+from .tutorial_docker import TutorialDocker
+from pathlib import Path
 
-PKG_PATH = _Path(__path__[0]) # type: ignore  # mypy issue #1422
+PKG_PATH = Path(__path__[0]) # type: ignore  # mypy issue #1422
 
-rand: _RandomHelper = None # TODO rename rand?
+rand: RandomHelper = None # TODO rename rand?
 """
 The RandomHelper which will be used when creating random files and folders. TutorialDocker will set it when it runs.
 Has to be at the package level, and so that File methods can access it.
 """
 
-_tutorial: _TutorialDocker = None
+_tutorial: TutorialDocker = None
 """
 Global that is set to the currently run tutorial, so that File methods can access it.
 """
+
+__all__ = [
+    "file",
+    "permissions",
+    "Puzzle",
+    "File",
+    "Permissions",
+    "change_user",
+    "rand",
+]
