c68b8a659dd584857cdbf289c412ee9986399fed
Jesse Hines <jessehines@southern.edu>
2021-06-14 12:43:17 -0500

    Code cleanup in GUI

--- shell_adventure/gui.py
+++ shell_adventure/gui.py
@@ -11,12 +11,6 @@ from .scrolled_frame import VerticalScrolledFrame
 from . import tutorial, PKG_PATH
 from shell_adventure_docker.support import Puzzle
 
-class WrappingLabel(ttk.Label):
-    """Label that automatically adjusts the wrap to the size"""
-    def __init__(self, master=None, **kwargs):
-        super().__init__(master, borderwidth = 0, **kwargs) # borderwidth = 0 fixes "chopping off" of pieces of letters at the edges
-        self.bind('<Configure>', lambda e: self.config(wraplength = e.width))
-
 class GUI(ThemedTk):
     def __init__(self, tutorial: tutorial.Tutorial, undo_callback: Callable): # TODO find a better way to restart the bash session
         """ Creates and launches the Shell Adventure GUI. Pass it the tutorial object. """
@@ -26,14 +20,13 @@ class GUI(ThemedTk):
         self.undo_callback = undo_callback
         self.student_cwd: PurePosixPath = None # The path to the student's current directory
         self.file_tree_root = PurePosixPath("/") # The root of the displayed file tree
-        self.icons = self.get_icons() # We have to keep a reference to the icons or they will get deleted
 
+        self.icons = self._get_icons() # We have to keep a reference to the icons or they will get deleted
         self.file_tree: ttk.Treeview = None
-        self.file_icons = self.get_file_icons()
+        self.file_icons = self._get_file_icons()
 
         self.time_label = StringVar(self, value = "Time: 00:00")
         self.score_label = StringVar(self, value = "Score: 0/10")
-        # self.flag_input = StringVar(self, value="")
 
         self.title("Shell Adventure")
         self.minsize(300, 100) # To keep you from being able to shrink everything off the screen.
@@ -43,16 +36,16 @@ class GUI(ThemedTk):
         self.rowconfigure(2, weight = 0)
         self.rowconfigure(3, weight = 0)
 
-        status_bar = self.make_status_bar(self)
+        status_bar = self._make_status_bar(self)
         status_bar.grid(row = 0, column = 0, sticky = "WE")
 
-        self.file_tree = self.make_file_tree(self)
+        self.file_tree = self._make_file_tree(self)
         self.file_tree.grid(row = 1, column = 0, sticky = "NSWE")
 
-        undo_frame, self.undo_button, self.restart_button = self.make_undo_frame(self)
+        undo_frame, self.undo_button, self.restart_button = self._make_undo_frame(self)
         undo_frame.grid(row = 2, column = 0, sticky = "NSWE")
 
-        self.puzzle_frame = self.make_puzzle_frame(self)
+        self.puzzle_frame = self._make_puzzle_frame(self)
         self.puzzle_frame.grid(row = 3, column = 0, sticky = "NSWE")
         self.update_puzzle_frame()
 
@@ -72,35 +65,33 @@ class GUI(ThemedTk):
 
         self.mainloop()
 
-    def get_icons(self) -> Dict[str, ImageTk.PhotoImage]: # TODO combine this and get_file_icons()
+    def _get_icons(self) -> Dict[str, ImageTk.PhotoImage]:
         """ Returns a map of icons. """
         icon_files = { 
             "undo": "undo.png",
             "restart": "restart.png",
+            "file": "file.png",
+            "file_symlink": "file_symlink.png",
+            "folder": "folder.png",
+            "folder_symlink": "folder_symlink.png",
         }
-        # fetch icons files. We have to save to a field or tkinter will lose the images somehow.
-        icons = {}
+        icons = {} # fetch icons files. We have to save to a field or tkinter will lose the images somehow.
         for key, file in icon_files.items():
             img = Image.open(PKG_PATH / "icons" / file).resize((16, 16), Image.ANTIALIAS)
             icons[key] = ImageTk.PhotoImage(img)
         return icons
 
-    def get_file_icons(self) -> Dict[Tuple[bool, bool], ImageTk.PhotoImage]:
+    def _get_file_icons(self) -> Dict[Tuple[bool, bool], ImageTk.PhotoImage]:
         """ Returns a map of icons representing 4 file types. Maps (is_dir, is_symlink) tuples to the icons """
         icon_files = { 
-            (False, False): "file.png",
-            (False, True ): "file_symlink.png",
-            (True,  False): "folder.png",
-            (True,  True ): "folder_symlink.png",
+            (False, False): "file",
+            (False, True ): "file_symlink",
+            (True,  False): "folder",
+            (True,  True ): "folder_symlink",
         }
-        # fetch icons files. We have to save to a field or tkinter will lose the images somehow.
-        file_icons = {}
-        for key, file in icon_files.items():
-            img = Image.open(PKG_PATH / "icons" / file).resize((16, 16), Image.ANTIALIAS)
-            file_icons[key] = ImageTk.PhotoImage(img)
-        return file_icons
+        return {tup: self.icons[icon] for tup, icon in icon_files.items()}
 
-    def make_status_bar(self, master):
+    def _make_status_bar(self, master):
         """ Returns the status bar that displays time and current score. """
         status_bar = ttk.Frame(master)
 
@@ -111,7 +102,22 @@ class GUI(ThemedTk):
         
         return status_bar
 
-    def make_undo_frame(self, master):
+    def _make_file_tree(self, master):
+        """ Returns the file view """
+        file_tree = ttk.Treeview(master, show="tree") # don't show the heading
+
+        file_tree.tag_configure("cwd", font = font.Font(weight="bold"))
+
+        def on_open(e):
+            item = file_tree.focus()
+            if not file_tree.tag_has("loaded", item):
+                self.load_folder(item)
+
+        file_tree.tag_bind("dir", "<<TreeviewOpen>>", on_open)
+
+        return file_tree
+
+    def _make_undo_frame(self, master):
         """ Returns a frame showing the undo and restart buttons and the undo and reset buttons themselves """
         undo_frame = ttk.Frame(master)
 
@@ -128,7 +134,16 @@ class GUI(ThemedTk):
         undo_button.grid(row = 0, column = 1)
 
         return (undo_frame, undo_button, restart_button)
-        
+
+    def _make_puzzle_frame(self, master) -> VerticalScrolledFrame:
+        """ Returns a frame container the puzzle list. """
+        # map puzzles to their question label and button. By default, Python will use object identity for dict keys, which is what we want.
+        puzzle_frame = VerticalScrolledFrame(master)
+        puzzle_frame.interior.columnconfigure(0, weight = 1)
+        puzzle_frame.interior.rowconfigure(0, weight = 1)
+
+        return puzzle_frame
+
     def start_timer_loop(self):
         """ Starts a loop which will update the timer every second. """
         self.after(1000, self.start_timer_loop)
@@ -140,30 +155,6 @@ class GUI(ThemedTk):
             self.time_label.set(f"Time: {hours}:{minutes:02}:{seconds:02}")
         else:
             self.time_label.set(f"Time: {minutes:02}:{seconds:02}")
-
-    def make_file_tree(self, master):
-        """ Returns the file view """
-        file_tree = ttk.Treeview(master, show="tree") # don't show the heading
-
-        file_tree.tag_configure("cwd", font = font.Font(weight="bold"))
-
-        def on_open(e):
-            item = file_tree.focus()
-            if not file_tree.tag_has("loaded", item):
-                self.load_folder(item)
-
-        file_tree.tag_bind("dir", "<<TreeviewOpen>>", on_open)
-
-        return file_tree
-
-    def make_puzzle_frame(self, master) -> VerticalScrolledFrame:
-        """ Returns a frame container the puzzle list. """
-        # map puzzles to their question label and button. By default, Python will use object identity for dict keys, which is what we want.
-        puzzle_frame = VerticalScrolledFrame(master)
-        puzzle_frame.interior.columnconfigure(0, weight = 1)
-        puzzle_frame.interior.rowconfigure(0, weight = 1)
-
-        return puzzle_frame
 
     def update_puzzle_frame(self):
         """ Remakes the puzzle list. """
@@ -289,3 +280,9 @@ class GUI(ThemedTk):
         self.destroy()
         # Raising the exception so that we can handle it in the launch script 
         raise val from None # none so we don't get "while handling..."
+
+class WrappingLabel(ttk.Label):
+    """Label that automatically adjusts the wrap to the size"""
+    def __init__(self, master=None, **kwargs):
+        super().__init__(master, borderwidth = 0, **kwargs) # borderwidth = 0 fixes "chopping off" of pieces of letters at the edges
+        self.bind('<Configure>', lambda e: self.config(wraplength = e.width))
