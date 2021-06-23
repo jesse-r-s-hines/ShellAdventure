d724610068ff469b9313a62b31cb247cf701b6e4
Jesse Hines <jessehines@southern.edu>
2021-06-22 20:12:44 -0500

    Add format_exc and format_user_exc

--- shell_adventure_docker/exceptions.py
+++ shell_adventure_docker/exceptions.py
@@ -1,11 +1,13 @@
 """ Contains various exception classes """
 import tblib.pickling_support
-import textwrap
+import textwrap, traceback
 
 __all__ = [
     "TutorialContainerStartupError",
     "UserCodeError",
     "TutorialConfigException",
+    "format_exc",
+    "format_user_exc",
 ]
 
 class TutorialContainerStartupError(Exception): # TODO move this class out of shell_adventure_docker?
@@ -22,6 +24,24 @@ class TutorialConfigException(Exception):
 class UserCodeError(Exception):
     """ Class for when user supplied code such as PuzzleGenerator's and AutoGrader's throw. """
 
+def format_exc(e: Exception) -> str:
+    """ Formats an exception with traceback. Wrapper for traceback.format_exception. """
+    lines = traceback.format_exception(type(e), e, e.__traceback__)
+    return "".join(lines)
+
+def format_user_exc(e: Exception) -> str:
+    """
+    Format an exception in user supplied code. Filters out our code from the traceback so we
+    can show only the relevant data to the user.
+    """
+    # See https://stackoverflow.com/questions/31949760/how-to-limit-python-traceback-to-specific-files
+    frames = [f for f in traceback.extract_tb(e.__traceback__) if f.filename == "<string>"]
+    lines = traceback.format_list(frames) + traceback.format_exception_only(type(e), e)
+    
+    return "Traceback (most recent call last):\n" + "".join(lines)
+
+# Maybe allow pickling the exceptions with additional fields?
+# See https://stackoverflow.com/questions/49715881/how-to-pickle-inherited-exceptions
 
 # Need to make sure that this is called before I try to pickle any exceptions and after any exceptions are defined
 tblib.pickling_support.install()
