3e311bb8e50998b4b3a17ea630c2d50522514e5e
Jesse Hines <jessehines@southern.edu>
2021-07-03 13:53:33 -0500

    More tweaks to launcher script

--- shell_adventure/shell_adventure.py
+++ shell_adventure/shell_adventure.py
@@ -4,44 +4,35 @@ from shell_adventure.gui import GUI
 from shell_adventure.tutorial import Tutorial
 from shell_adventure_shared.tutorial_errors import *
 
-def print_error_header(message: str):
-    print(f"\n\n{f' {message} '.center(60, '=')}\n")
-
-def print_logs(tutorial: Tutorial):
-    if tutorial.logs():
-        print("Container Logs:\n" + indent(tutorial.logs(), "  "))
-
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("No tutorial config file given.")
-        exit(1)
+        exit("No tutorial config file given.")
 
     try:
         tutorial = Tutorial(sys.argv[1])
     except ConfigError as e:
-        print(e)
-        exit(1)
+        exit(e)
     
+    def exit_and_log(message, header: str = None, ):
+        """ Print error string and exit. Prints container logs and an optional header. """
+        if header:
+            print(f"\n\n{f' {header} '.center(60, '=')}\n")
+        print(message)
+        if tutorial.logs():
+            print("Container Logs:\n" + indent(tutorial.logs(), "  "))
+        exit(1)
+
     try:
         with tutorial: # Sets up the container with the tutorial inside, context manager will remove container
             tutorial.attach_to_shell()
             gui = GUI(tutorial, restart_callback = tutorial.attach_to_shell)
     except ConfigError as e: # We can get config errors when starting the container as well
-        print(e)
-        exit(1)
+        exit(e)
     except UnhandledError as e: # Our code in the container broke
-        print_error_header("An unhandled error occurred in the container")
-        print(format_exc(e))
-        print_logs(tutorial)
-        exit(-1)
+        exit_and_log(format_exc(e), header = "An unhandled error occurred in the container")
     except TutorialError as e: # User caused errors or environment errors
-        print_error_header("An error occurred")
-        print(e)
-        exit(2)
+        exit_and_log(e, header = "An error occurred")
     except Exception as e: # Our code broke
-        print_error_header("An unhandled error occurred")
-        print(format_exc(e))
-        print_logs(tutorial)
-        exit(-1)
+        exit_and_log(format_exc(e), header = "An unhandled error occurred")
     finally:
         print() # Add newline so that the terminal's next program is on a line by itself properly.
