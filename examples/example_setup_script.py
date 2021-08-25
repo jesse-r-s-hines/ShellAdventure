32a2ccc3fef35e7588c06dc4992364f7c110c357
Jesse Hines <jessehines@southern.edu>
2021-08-25 15:00:32 -0400

    Add examples for setup_scripts

---
+++ examples/example_setup_script.py
@@ -0,0 +1,4 @@
+from shell_adventure.api import *
+
+# You can create files, run commands or anything else you can do in puzzle generation in a setup script
+File("README").create(content = "Welcome to this Linux tutorial")
